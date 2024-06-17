c5d2692106fb74f94bc29ea4b0f86d8cb595d8e1
JaiPannu-IITI <pannujai06iiti@icloud.com>
2024-06-18 04:55:39 +0530

    dummy navbar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Emogrid from "./components/emogrid/emogrid";
 
+
+
 export default function Home() {
   return (
-    <>home
-    {/* <Emogrid/> */}
+    <>
+   home
     </>
     
    
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,15 +1,34 @@
 import type { Config } from "tailwindcss";
 
 const {nextui} = require("@nextui-org/react");
+const { colors: defaultColors } = require('tailwindcss/defaultTheme')
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+   
+    extend: {
+
+      "colors":{ 
+        "customorange": {
+            "700": "#F86400",
+            "600": "#F79500",
+            "500": "#EBB652",
+        },
+        "customblue": {
+            "600": "#033B55",
+            "500": "#0E65A3",
+            
+        },
+      },
+
+
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
