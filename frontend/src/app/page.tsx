d6b992dcf03aa2031e6109ad996cd4a063375272
JaiPannu-IITI <pannujai06iiti@icloud.com>
2024-06-20 23:36:34 +0530

    added loading screen

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,30 @@
+"use client";
 import Image from "next/image";
 import Emogrid from "./components/emogrid/emogrid";
-
-
+import Loader from "./components/loader/loading";
+import { useEffect, useState } from "react";
+import Navbar from "./navbar/navbar";
+import Logo from "./components/logo/logo"
 
 export default function Home() {
+ 
+
+  const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 5000)
+    }, [])
+    if (loading) {
+        return <Loader/>
+    }
   return (
-    <>
-   home
-    </>
-    
-   
-   
+    <div className="App">
+       <Navbar />
+       <Logo width={500}/>
+      <h1>Hello</h1>
+    </div>
   );
 }
+   
+   
+  
+
