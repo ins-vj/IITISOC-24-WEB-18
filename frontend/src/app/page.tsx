e6b9c3005a0b2a528e188bf338d9c93a1d821a92
princekumarofficial <x.prince.x@outlook.com>
2024-06-21 18:58:38 +0530

    folder structure

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,42 +1,27 @@
 "use client";
 import Image from "next/image";
-import Emogrid from "./components/emogrid/emogrid";
+import Emogrid from "@/components/emogrid/emogrid";
 
-import Loader from "./components/loader/loading";
+import Loader from "../components/loader/loading";
 import { useEffect, useState } from "react";
-import Navbar from "./navbar/navbar";
-import Logo from "./components/logo/logo"
+import Navbar from "@/components/navbar/navbar";
+import Logo from "../components/logo/logo";
 
-import Hero from "./components/hero/hero";
-
-
+import Hero from "../components/hero/hero";
 
 export default function Home() {
- 
-
-  const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        setTimeout(() => setLoading(false), 5000)
-    }, [])
-    if (loading) {
-        return <Loader/>
-    }
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 5000);
+  }, []);
+  if (loading) {
+    return <Loader />;
+  }
   return (
-
     <div className="App">
-       <Navbar />
-     
-       <Hero ></Hero>
+      <Navbar />
+
+      <Hero></Hero>
     </div>
-
-  
-    
-   
-   
-
   );
 }
-   
-   
-  
-
