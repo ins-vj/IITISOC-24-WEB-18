aa20f2a81239f7c8bb5ca959f3f7d2ef7558f08f
Jai Pannu <142983705+JaiPannu-IITI@users.noreply.github.com>
2024-06-23 02:03:41 +0530

    Delete frontend directory

--- frontend/src/components/Buttons/ButtonComponent.tsx
+++
@@ -1,7 +0,0 @@
-import {Button} from "@nextui-org/react";
-import styles from "./ButtonComponent.module.css";
-export const ButtonComponent = () => {
-    return (
-        <Button color="primary" className={styles.special}>Join MEET now</Button>
-    )
-}
