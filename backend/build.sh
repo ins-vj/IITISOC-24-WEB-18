d0bac1a32477472332e7e533f6248ea63c41d346
princekumarofficial <x.prince.x@outlook.com>
2024-07-23 19:26:14 +0530

    minor change

--- backend/build.sh
+++ backend/build.sh
@@ -2,12 +2,10 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-
 python manage.py makemigrations api users messagesio videocall
 
 python manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]];
 then
-  python manage.py createsuperuser --no-input
+  python manage.py createsuperuserfi
