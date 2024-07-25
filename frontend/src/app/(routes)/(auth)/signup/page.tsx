6cbcb453c5363c28ab8e39c8a39fa7d2e69d514d
princekumarofficial <x.prince.x@outlook.com>
2024-07-25 18:07:44 +0530

    update

--- frontend/src/app/(routes)/(auth)/signup/page.tsx
+++ frontend/src/app/(routes)/(auth)/signup/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import Link from "next/link";
-import Particle from "@/components/auth/signup/particle";
+import Particle from "@/components/signup/particle";
 import Image from "next/image";
-import Card from "@/components/auth/signup/card";
+import Card from "@/components/signup/card";
 
 export default function SignUp() {
   return (
