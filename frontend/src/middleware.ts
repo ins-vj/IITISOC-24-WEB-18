3239cf40646b8a101f07ad787a1ba6dbe56a87c7
princekumarofficial <x.prince.x@outlook.com>
2024-07-27 19:19:41 +0530

    middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,14 +1,22 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 
 export default function middleware(request: NextRequest) {
   const userCookie = request.cookies.get("accessToken");
   if (!userCookie) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
+  if (request.nextUrl.pathname.startsWith("login")) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/videocall/:path*", "/call/:path*"],
+  matcher: [
+    "/videocall/:path*",
+    "/call/:path*",
+    "/dashboard/:path*",
+    "/dashboard",
+  ],
 };
