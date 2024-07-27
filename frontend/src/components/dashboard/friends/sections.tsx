17beb9f29b5a5f138bf5e6e7c99539a53e29497e
samnotfound404 <cse230001070@iiti.ac.in>
2024-07-27 23:59:50 +0530

    fix

--- frontend/src/components/dashboard/friends/sections.tsx
+++ frontend/src/components/dashboard/friends/sections.tsx
@@ -3,7 +3,7 @@
 
 export default function Sections(props:any){
     return(
-        <div className="  2xl:min-w-[600px]    xl:min-w-[400px] min-w-[350px] [@media(max-width:1100px)]:min-w-[95%] flex flex-col gap-[10px] items-center justify-around">
+        <div className="  2xl:min-w-[600px]    xl:min-w-[400px] min-w-[350px] [@media(max-width:1100px)]:min-w-[95%] flex flex-col gap-[10px] items-center justify-start">
         {props.children}
 
         </div>
